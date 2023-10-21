9eb5a58c4cc827ad96383e7c5d26b1f132a32272
Eecodev <mnasser.bbk@gmail.com>
2023-10-21 12:52:32 +0200

    insertNumber function created and operationalised

--- script.js
+++ script.js
@@ -7,17 +7,50 @@
 */
 
 
-const buttonInsertNumber = document.querySelector('.btn');
-
-let min = 1;
-let max = 100;
+const buttonSubmitNumber = document.querySelector('.btn');
+const textCont = document.getElementById('text-container');
+const boxNum = document.getElementById('box-number');
 const randArrayList = [];
-let correctNum = [];
+let correctNum = []
+let insertedNumber;
 
-buttonInsertNumber.addEventListener('click',generateNumbers);
+buttonSubmitNumber.addEventListener('click',generateNumbers);
 
 function generateNumbers(){
-    for(let i = 0; i <= 5; i++){
-        arrayList.push(getRndInteger(1,100));
+    for(let i = 0; i < 5; i++){
+        let randNum = getRndInteger(1, 99);
+        randArrayList.push(randNum);
+        console.log(randArrayList);
+        textCont.innerHTML = 'Memorise the numbers shown on the box, you have 30 seconds';
+        boxNum.innerHTML += `${randNum}, `;
+    }
+}
+setTimeout(insertNumber, 30000);
+
+
+function insertNumber(){
+    boxNum.classList.add('hidden');
+    for(let i = 1; i < 5; i++){
+        let insertedNumber = parseInt(prompt('Type the numbers'));
+
+        if (randArrayList.includes(insertedNumber)) {
+            correctNum.push(insertedNumber);
+
+            textCont.innerHTML="Numbers you have guessed are:";
+            boxNum.innerHTML = ` ${correctNum}, `;
+            boxNum.classList.remove("hidden");
+        
+        } else {
+            console.log('oh no!');
+            console.log(correctNum);
+        }
+
+        if(correctNum.length === 5){
+            textCont.innerHTML = 'Good job! You have guessed all the numbers';
+            console.log('they match');
+        } else if (correctNum.length === 0){
+            textCont.innerHTML = 'Oh no, you haven not guessed any numbers';
+            console.log('they don\'t match');
+        }
     }
 }
